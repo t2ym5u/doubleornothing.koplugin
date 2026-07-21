b04fc3466e0fe1e8b312b7d068076803223f8e8d
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-07-21 13:39:56 +0200

    chore: bump version to 1.0.2

--- _meta.lua
+++ _meta.lua
@@ -3,5 +3,5 @@ return {
     name        = "doubleornothing",
     fullname    = _("Double or Nothing Party"),
     description = _("Quiz where each team can bank their points or double the pot on a harder question."),
-    version     = "1.0.1",
+    version     = "1.0.2",
 }
