c0fd2085f841e8ad86cc3f36bb6f8d88ead7063e
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-08-05 15:00:08 +0200

    feat: add ES and DE translations

--- i18n_fr.lua
+++ i18n_fr.lua
@@ -1,4 +1,4 @@
 return {
-    ["Double or Nothing Party"] = { fr = "Double or Nothing Party" },
-    ["Quiz where each team can bank their points or double the pot on a harder question."] = { fr = "Quiz où chaque équipe peut encaisser ses points ou doubler la cagnotte sur une question plus difficile." },
+    ["Double or Nothing Party"] = { fr = "Double or Nothing Party", es = "Double or Nothing Party", de = "Double or Nothing Party" },
+    ["Quiz where each team can bank their points or double the pot on a harder question."] = { fr = "Quiz où chaque équipe peut encaisser ses points ou doubler la cagnotte sur une question plus difficile.", es = "Quiz donde cada equipo puede asegurar sus puntos o doblar el bote con una pregunta más difícil.", de = "Quiz, bei dem jedes Team seine Punkte sichern oder den Einsatz mit einer schwierigeren Frage verdoppeln kann." },
 }
